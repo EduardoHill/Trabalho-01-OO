12aae443435608a5ba24f7a04ec619f1c9c17f30
Eduardo Hill <eduardohillfav@gmail.com>
2025-10-19 15:41:22 -0300

    feat: Criando classes dos personagens

---
+++ src/main/java/br/ufjf/dcc/dcc025/westerosbattle/model/entities/Character.java
@@ -0,0 +1,4 @@
+package br.ufjf.dcc.dcc025.westerosbattle.model.entities;
+
+public abstract class Character {
+}
---
+++ src/main/java/br/ufjf/dcc/dcc025/westerosbattle/model/entities/Targaryen.java
@@ -0,0 +1,4 @@
+package br.ufjf.dcc.dcc025.westerosbattle.model.entities;
+
+public class Targaryen {
+}
