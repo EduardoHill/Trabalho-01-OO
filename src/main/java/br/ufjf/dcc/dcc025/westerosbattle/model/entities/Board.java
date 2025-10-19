1eb432228418b22b690bef521c34e1b25e3f3caf
caiobertelli483 <02163846644@estudante.ufjf.br>
2025-10-19 16:32:57 -0300

    feat: Criando método de mover e criando Enum de direção

--- src/main/java/br/ufjf/dcc/dcc025/westerosbattle/model/entities/Board.java
+++ src/main/java/br/ufjf/dcc/dcc025/westerosbattle/model/entities/Board.java
@@ -1,11 +1,28 @@
 package br.ufjf.dcc.dcc025.westerosbattle.model.entities;
 
+import br.ufjf.dcc.dcc025.westerosbattle.model.enums.Direction;
+
+import java.util.Random;
+
 public class Board {
-    private int[][] board;
+    private Integer[][] board;
     private final int size = 10;
 
-    public Board(int size) {
-        board = new int[size][size];
+    public Board() {
+        board = new Integer[size][size];
+    }
+
+    public void setPosition(Character character, boolean side) {
+        Random rand = new Random();
+        int line, column;
+        do {
+            line = rand.nextInt(size);
+            column = side ? rand.nextInt(size / 2) : rand.nextInt(size / 2, size);
+        } while (board[line][column] != null);
+    }
+
+    public void move(Character character, Direction direction) {
+
     }
 
 }
---
+++ src/main/java/br/ufjf/dcc/dcc025/westerosbattle/model/enums/Direction.java
@@ -0,0 +1,7 @@
+package br.ufjf.dcc.dcc025.westerosbattle.model.enums;
+
+public enum Direction {
+
+    DIREITA,ESQUERDA,CIMA,BAIXO,DIREITA_CIMA,DIREITA_BAIXO,ESQUERDA_CIMA,ESQUERDA_BAIXO;
+
+}
