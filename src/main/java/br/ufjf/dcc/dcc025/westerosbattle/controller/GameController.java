7bab65a06e822bf37e26201bf57847fa9a51eac1
Eduardo Hill <eduardohillfav@gmail.com>
2025-10-20 11:17:28 -0300

    refactor: Alterando estrutura de pastas

--- src/main/java/br/ufjf/dcc/dcc025/westerosbattle/controller/GameController.java
+++ src/main/java/br/ufjf/dcc/dcc025/westerosbattle/controller/GameController.java
@@ -1,16 +1,20 @@
-package br.ufjf.dcc.dcc025.westerosbattle.model.entities;
+package br.ufjf.dcc.dcc025.westerosbattle.controller;
+
+import br.ufjf.dcc.dcc025.westerosbattle.model.entities.Board;
+import br.ufjf.dcc.dcc025.westerosbattle.model.entities.Character;
+import br.ufjf.dcc.dcc025.westerosbattle.model.entities.GameLog;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Game {
+public class GameController {
     private Board board;
     private GameLog gameLog;
     private int currentTurn;
     private List<Character> characters;
 
 
-    public Game() {
+    public GameController() {
         this.board = new Board();
         this.gameLog = new GameLog();
         this.currentTurn = 1;
