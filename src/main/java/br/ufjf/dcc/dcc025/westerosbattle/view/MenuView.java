3076dd36d198dd08cca5c9ac5a59208a8f4c13c7
EduardoHill <eduardohillfav@gmail.com>
2025-10-24 15:17:25 -0300

    feat: criando alguns metodos do menu

---
+++ src/main/java/br/ufjf/dcc/dcc025/westerosbattle/view/MenuView.java
@@ -0,0 +1,61 @@
+package br.ufjf.dcc.dcc025.westerosbattle.view;
+
+import br.ufjf.dcc.dcc025.westerosbattle.model.enums.Direction;
+import br.ufjf.dcc.dcc025.westerosbattle.model.entities.Character;
+
+import java.util.List;
+
+public class MenuView {
+    private final InputView input;
+
+
+    public MenuView(InputView input) {
+        this.input = input;
+    }
+
+    public void showWelcome(){
+        System.out.println("\n======================================================");
+        System.out.println("       A Batalha Tática das Casas de Westeros         ");
+        System.out.println("======================================================");
+    }
+
+    public int mainMenu(){
+        System.out.println("Menu Principal");
+        System.out.println("[1] Novo jogo");
+        System.out.println("[2] Sair");
+        return input.readChoice("Escolha: " , 1, 2);
+    }
+
+    public int chooseCharacter(){
+        System.out.println("Escolha um personagem: ");
+        System.out.println("[1] Stark     (HP:60, DEF:10, ATK:20, RANGE: 1, EXP:DEF-Bruta ");
+        System.out.println("[1] Lannister     (HP:50, DEF:10, ATK:20, RANGE: 2, EXP:ATK-LIQUIDO ");
+        System.out.println("[1] Targaryen     (HP:45, DEF:10, ATK:20, RANGE: 3, EXP:ATK-BRUTO ");
+        return input.readChoice("Escolher: ", 1, 3);
+    }
+
+    public int chooseAction(){
+        System.out.println("Escolha uma ação: ");
+        System.out.println("[1] Mover");
+        System.out.println("[2] Atacar");
+        return input.readChoice("Escolher: ", 1, 2);
+    }
+
+    public Direction chooseDirection(){
+        return input.readDirection();
+    }
+
+    public Character chooseTarget(List<Character> targets){
+        System.out.println("Escolha um alvo: ");
+        for (int i = 0 ; i < targets.size(); i++){
+
+            Character character = targets.get(i);
+            System.out.println("[" + (i + 1) + "] " + character.getName() + " HP:" + character.getHealth() );
+        }
+        int aux = input.readChoice("Escolha seu personagem", 1, targets.size());
+        return targets.get(aux-1);
+    }
+
+
+
+}
