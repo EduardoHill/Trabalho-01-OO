d5eb49b7eebb6016dc847b25f86086840b794d3c
caiobertelli483 <02163846644@estudante.ufjf.br>
2025-10-27 18:14:37 -0300

    feat:Implementando PlayerAction

--- src/main/java/br/ufjf/dcc/dcc025/westerosbattle/model/strategy/PlayerAction.java
+++ src/main/java/br/ufjf/dcc/dcc025/westerosbattle/model/strategy/PlayerAction.java
@@ -1,4 +1,40 @@
 package br.ufjf.dcc.dcc025.westerosbattle.model.strategy;
 
-public class PlayerAction {
+import br.ufjf.dcc.dcc025.westerosbattle.model.entities.Board;
+import br.ufjf.dcc.dcc025.westerosbattle.model.entities.Character;
+import br.ufjf.dcc.dcc025.westerosbattle.model.enums.ActionType;
+import br.ufjf.dcc.dcc025.westerosbattle.model.enums.Direction;
+import br.ufjf.dcc.dcc025.westerosbattle.view.MenuView;
+
+import java.util.List;
+
+public class PlayerAction{
+    private ActionType actionType;
+    private Direction direction;
+    private Character target;
+
+    public PlayerAction(ActionType actionType, Direction direction) {
+        this.actionType = actionType;
+        this.direction = direction;
+        this.target = null;
+    }
+
+    public PlayerAction(ActionType actionType, Character target) {
+        this.actionType = actionType;
+        this.target = target;
+        this.direction = null;
+    }
+
+    public ActionType getActionType() {
+        return actionType;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public Character getTarget() {
+        return target;
+    }
 }
+
--- src/main/java/br/ufjf/dcc/dcc025/westerosbattle/model/strategy/PlayerSrategy.java
+++ src/main/java/br/ufjf/dcc/dcc025/westerosbattle/model/strategy/PlayerSrategy.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface PlayerSrategy {
     PlayerAction decideTurn(Character player, List<Character> enemies, Board board);
+
+    PlayerAction decideTurn(br.ufjf.dcc.dcc025.westerosbattle.model.entities.Character player, List<br.ufjf.dcc.dcc025.westerosbattle.model.entities.Character> enemies, Board board);
 }
