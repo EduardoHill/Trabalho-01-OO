5317e863179c505255841ad35c335abbc37579e4
caiobertelli483 <02163846644@estudante.ufjf.br>
2025-10-27 18:23:35 -0300

    feat: criando a Human Strategy

--- src/main/java/br/ufjf/dcc/dcc025/westerosbattle/model/strategy/HumanStrategy.java
+++ src/main/java/br/ufjf/dcc/dcc025/westerosbattle/model/strategy/HumanStrategy.java
@@ -1,4 +1,38 @@
 package br.ufjf.dcc.dcc025.westerosbattle.model.strategy;
 
-public class HumanStrategy {
+import br.ufjf.dcc.dcc025.westerosbattle.model.entities.Board;
+import br.ufjf.dcc.dcc025.westerosbattle.model.entities.Character;
+import br.ufjf.dcc.dcc025.westerosbattle.model.enums.ActionType;
+import br.ufjf.dcc.dcc025.westerosbattle.model.enums.Direction;
+import br.ufjf.dcc.dcc025.westerosbattle.view.MenuView;
+
+import java.util.List;
+
+public class HumanStrategy implements PlayerStrategy {
+    private MenuView menu;
+
+    public HumanStrategy(MenuView menu) {
+        this.menu = menu;
+    }
+
+
+    @Override
+    public PlayerAction decideTurn(Character player, List<Character> enemies, Board board) {
+        int action = menu.chooseAction();
+
+        if(action == 1){
+            Direction dir = menu.chooseDirection();
+            return new PlayerAction(ActionType.MOVE, dir);
+        } else {
+            List<Character> enemiesAlive = enemies.stream().filter(Character::isAlive).toList();
+
+            if(enemiesAlive.isEmpty()){
+                System.out.println("Nenhum inimigo vivo!");
+                return null;
+            }
+
+            Character target = menu.chooseTarget(enemiesAlive);
+            return new PlayerAction(ActionType.ATTACK, target);
+        }
+    }
 }
--- src/main/java/br/ufjf/dcc/dcc025/westerosbattle/model/strategy/PlayerStrategy.java
+++ src/main/java/br/ufjf/dcc/dcc025/westerosbattle/model/strategy/PlayerStrategy.java
@@ -1,11 +1,13 @@
 package br.ufjf.dcc.dcc025.westerosbattle.model.strategy;
 
+
 import br.ufjf.dcc.dcc025.westerosbattle.model.entities.Board;
+import br.ufjf.dcc.dcc025.westerosbattle.model.entities.Character;
 
 import java.util.List;
 
-public interface PlayerSrategy {
+public interface PlayerStrategy {
+
     PlayerAction decideTurn(Character player, List<Character> enemies, Board board);
 
-    PlayerAction decideTurn(br.ufjf.dcc.dcc025.westerosbattle.model.entities.Character player, List<br.ufjf.dcc.dcc025.westerosbattle.model.entities.Character> enemies, Board board);
 }
